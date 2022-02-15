f9d1e78c8885bb3868a3dd73c4dfa9675ce4d7d5
mahesh chomal <maheshchomal@caperminttechnologies.com>
2022-02-15 13:07:18 +0530

    user table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,25 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('middle_name')->nullable();
+            $table->string('gender')->nullable();
+            $table->string('date_of_birth')->nullable();
+            $table->string('home_address')->nullable();
+            $table->string('office_address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('state')->nullable();
+            $table->string('state_of_origin')->nullable();
+            $table->string('phone_num')->nullable();
             $table->string('email')->unique();
+            $table->string('otp')->nullable();
+            $table->tinyInteger('is_verified')->default(0);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('avatar')->nullable();
+            $table->string('file_img')->nullable();
+            $table->tinyInteger('active')->default(1);
             $table->rememberToken();
             $table->timestamps();
         });
